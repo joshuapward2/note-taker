c592e5d139b9fdf8257fda4dbaf1b027e3ab92d4
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-08-25 16:45:43 -0400

    trying different approaches

--- Develop/server.js
+++ Develop/server.js
@@ -2,16 +2,30 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
-
-
-
 
 
 
 app.get('/notes.html',(req, res) => {
+    reg.
     res.send('Hello World!');
+    res.render('notes')
 });
+
+app.get('/index.html',(req, res) => {
+    reg.
+    res.send('Hello World!');
+    res.render('index')
+});
+
+app.use(express.static('public'));
+
+
+app.get('/api/notes', (req, res) => {
+    req.
+
+});
+
+app.post('/api/notes', )
 
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
