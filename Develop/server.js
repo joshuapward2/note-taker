3788eab58ccdc21467c446ee7549159a302c4b92
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-08-25 18:16:23 -0400

    notes route set

--- Develop/server.js
+++ Develop/server.js
@@ -1,31 +1,46 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const notes = require('./db/db.json');
+const path = require('path');
+
+let randomId = Math.floor(Math.random()*10000000)
+
+//HTML Routes
 
 
-
-
-app.get('/notes.html',(req, res) => {
-    reg.
-    res.send('Hello World!');
-    res.render('notes')
+app.get('/notes',(req, res) => {
+    
+    res.sendFile(path.join( __dirname, 'notes.html'));
+  
 });
 
-app.get('/index.html',(req, res) => {
-    reg.
-    res.send('Hello World!');
-    res.render('index')
+app.get('/',(req, res) => {
+    
+    res.sendFile(path.join( __dirname, 'index.html'));
+  
 });
+  
+
 
 app.use(express.static('public'));
 
 
-app.get('/api/notes', (req, res) => {
-    req.
+//Api Routes
 
+
+app.get('/api/notes', (req, res) => {
+    res.json()
 });
 
-app.post('/api/notes', )
+
+app.post('/api/notes', (req, res) =>{
+
+} )
+
+
+
+
 
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
